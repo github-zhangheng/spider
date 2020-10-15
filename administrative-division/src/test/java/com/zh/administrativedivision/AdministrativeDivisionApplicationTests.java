55d16c1f6adb92c453574701c324d3a98e78ae4a
zhangheng <honorablezhangheng@163.com>
2020-10-15 14:59:04 +0800

    optimize

--- administrative-division/src/test/java/com/zh/administrativedivision/AdministrativeDivisionApplicationTests.java
+++ administrative-division/src/test/java/com/zh/administrativedivision/AdministrativeDivisionApplicationTests.java
@@ -5,19 +5,19 @@ import com.zh.administrativedivision.service.AreaService;
 import com.zh.administrativedivision.webmagic.pipeline.AreaPipeline;
 import com.zh.administrativedivision.webmagic.processor.AreaProcessor;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import us.codecraft.webmagic.Spider;
 import us.codecraft.webmagic.scheduler.QueueScheduler;
 
+import javax.annotation.Resource;
 import java.util.ArrayList;
 import java.util.List;
 
 @SpringBootTest
 class AdministrativeDivisionApplicationTests {
-    @Autowired
+    @Resource
     private AreaService areaService;
-    @Autowired
+    @Resource
     private AreaPipeline areaPipeline;
 
     @Test
@@ -29,9 +29,10 @@ class AdministrativeDivisionApplicationTests {
     @Test
     void saveArea() {
         Area area = new Area();
-        area.setAreaId("4");
-        area.setAreaName("5");
-        area.setAreaCode("6");
+        area.setAreaId("1");
+        area.setAreaName("2");
+        area.setAreaCode("3");
+        area.setAreaParentCode("3_P");
         area.setAreaLevel(1);
 
         areaService.saveArea(area);
@@ -40,14 +41,17 @@ class AdministrativeDivisionApplicationTests {
     @Test
     void saveAreas() {
         Area area1 = new Area();
-        area1.setAreaId("7");
-        area1.setAreaName("8");
-        area1.setAreaCode("9");
+        area1.setAreaId("4");
+        area1.setAreaName("5");
+        area1.setAreaCode("6");
+        area1.setAreaParentCode("6_P");
         area1.setAreaLevel(1);
 
         Area area2 = new Area();
-        area2.setAreaId("10");
-        area2.setAreaName("11");
+        area2.setAreaId("7");
+        area2.setAreaName("8");
+        area1.setAreaCode("9");
+        area1.setAreaParentCode("9_P");
         area2.setAreaLevel(1);
 
         List<Area> areaList = new ArrayList<>();
@@ -59,31 +63,34 @@ class AdministrativeDivisionApplicationTests {
 
     @Test
     void saveProvinceAreas() {
-        // 测试页面地址
-        // String indexUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/21.html";
-        // String indexUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/21/2101.html";
-        // String indexUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/21/01/210102.html";
-        // String indexUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/21/01/02/210102001.html";
+        // 数据均为2019年数据，测试三种情况
+        // 1：爬取页为首页，则爬取全国各个地区
+        // 2：爬取页为直辖市，则爬取该直辖市
+        // 3：爬取页为省，则爬取该省
+        // 区划编码首页
+        // String startUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/index.html";
 
-        // 存在部分汉字乱码的页面地址（只列出一部分测试）
-        // 该页面有个字“冮“（音：gāng），乱码
-        // String indexUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/21/07/82/210782101.html";
-        // 该页面有个字“牤“（音：māng），乱码
-        String indexUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/21/07/81/210781112.html";
+        // 北京市
+        // String startUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/11.html";
+        // String startUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/11/1101.html";
+        // String startUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/11/01/110101.html";
+        // String startUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/11/01/01/110101001.html";
 
+        // 河北省
+        // String startUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/13.html";
+        // String startUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/13/1301.html";
+        // String startUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/13/01/130102.html";
+        // String startUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/13/01/02/130102001.html";
 
-        // 2020年9月29日国家统计局行政区划代码2019年版首页地址
-        // String indexUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/index.html";
+        // 需要爬取的区划编码
+        String startUrl = "http://www.stats.gov.cn/tjsj/tjbz/tjyqhdmhcxhfdm/2019/51/5107.html";
 
-        // 要爬取的省行政区划代码前两位
-        String provinceAreaCode = "21";
-        Spider.create(new AreaProcessor(indexUrl, provinceAreaCode))
-                .addUrl(indexUrl)
+        Spider.create(new AreaProcessor("GB18030", startUrl))
+                .addUrl(startUrl)
                 .setScheduler(new QueueScheduler())
                 // 保存到数据库
-                // .addPipeline(areaPipeline)
-                .thread(1)
+                .addPipeline(areaPipeline)
+                .thread(5)
                 .run();
     }
-
 }
